efd5bc485eb8823c3e1f82ac7f34c00f9a84fcb9
AridTag <AridTag@binarydepths.com>
2018-05-03 22:47:42 -0400

    Added tab completion for Set-TargetFolder (alias: g)

--- profile.d/goToFolder.alias.ps1
+++ profile.d/goToFolder.alias.ps1
@@ -1,9 +1,37 @@
 $folderList = Import-Csv("goToFolder.config")
+$aliases = $folderList | Select-Object -ExpandProperty alias
 
 function Set-TargetFolder() {
-    Param($folderAlias)
-    $targetFolder = $folderList | Where-Object { $_.alias -eq $folderAlias }
-    Set-Location $targetFolder.folder
+    [CmdletBinding()]
+    Param()
+        DynamicParam {
+            $paramDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            
+            $parameterName = 'folderToAlias'
+            $parameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $parameterAttribute.Mandatory = $true
+            $parameterAttribute.Position = 0
+            $attributeCollection.Add($parameterAttribute)
+            
+            $validateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($aliases)
+            $attributeCollection.Add($validateSetAttribute)
+
+            $runtimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($parameterName, [string], $attributeCollection)
+            $paramDictionary.Add($runtimeParameter.Name, $runtimeParameter)
+            
+
+            return $paramDictionary
+        }
+
+        begin {
+            $folderAlias = $PSBoundParameters[$ParameterName]
+        }
+
+        process {
+            $targetFolder = $folderList | Where-Object { $_.alias -eq $folderAlias }
+            Set-Location $targetFolder.folder
+        }
 }
 
 Set-Alias -Name g -Value Set-TargetFolder
